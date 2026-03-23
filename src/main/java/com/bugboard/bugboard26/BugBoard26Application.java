5df0cace62f62a399ef3c90ad7128d0c74658f90
AntonioSoritto <antonio_soritto2016@libero.it>
2026-03-23 19:37:44 +0100

    .

--- src/main/java/com/bugboard/bugboard26/BugBoard26Application.java
+++ src/main/java/com/bugboard/bugboard26/BugBoard26Application.java
@@ -8,14 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BugBoard26Application {
 
     public static void main(String[] args) {
-        // DEVE stare qui, prima di tutto il resto
+
         System.setProperty("java.awt.headless", "false");
 
         SpringApplication app = new SpringApplication(BugBoard26Application.class);
-        app.setHeadless(false);  // ← disabilita headless anche su Spring
+        app.setHeadless(false);
         app.run(args);
-
-        SwingApp.launch();
     }
 }
 
